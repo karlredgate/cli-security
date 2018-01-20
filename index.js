5263ef572966e23d1fe8d54736cfb65295685e0a
Karl Redgate <Karl.Redgate@gmail.com>
2018-01-20 12:12:04 -0500

    connect new appsectool to module

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 module.exports = {
     WebSite: require('./src/website'),
 };
+
+module.exports.AppSecTool = require('./src/AppSecTool');
+
+/* vim: set autoindent expandtab sw=4 syntax=javascript: */
